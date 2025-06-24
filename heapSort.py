b25fbf74e498ada40daad94175e9bb75e4d161d6
Guilherme Santos <75951898+gui-HS@users.noreply.github.com>
2025-06-24 15:36:41 -0300

    Corrigido heapSort.py

--- heapSort.py
+++ heapSort.py
@@ -3,39 +3,41 @@
 #https://www.ime.usp.br/~pf/analise_de_algoritmos/aulas/hpsrt.html Resource
 #root = 0. right = 2*i, left = 2*i + 1, Parents = i/2
 
-vetor = [30, 15, 19, 10, 18, 25]
-vetor2 = [5,25,432,10,19,23]
+vetor = [5,25,432,10,19,23]
 
-def hipfy(arr, i):
-    n = len(arr)-1
+def heapify(arr, n, i):
     j = i
 
-    while 2*j <= n:
-        f = j*2
-        if f < n and arr[f] < arr[f+1]:
+    while (2*j+1) < n:
+        f = j*2+1
+
+        #Checa se esquerda é maior que direita
+        if f+1 < n and arr[f] < arr[f+1]:
             f = f+1
         
+        #Termina se o elemento pai for maior que filhos
         if arr[j] >= arr[f]:
             j = n
         else:
+            #Troca pai e nó de lugar
             arr[j], arr[f] = arr[f], arr[j]
+            j = f
 
 def constroi_max_heap(arr):
     n = len(arr)
 
     for i in range(n//2-1, -1, -1):
-        hipfy(arr, i)
+        heapify(arr, n, i)
 
 
 def heapsort(arr):
     constroi_max_heap(arr)
     n = len(arr)
 
-    for i in range(n-1, 1, -1):
-        arr[0] = arr[i]
-        hipfy(arr, 1)
+    for i in range(n-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0] 
+        heapify(arr, i, 0)
 
-heapsort(vetor2)
-print(vetor2)
-
+heapsort(vetor)
+print(vetor)
 
